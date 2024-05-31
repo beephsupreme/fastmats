340c1b685ce03daf512c355336266c7f961b2f9e
Michael Rowsey <mike@rowsey.org>
2024-05-31 15:50:52 -0700

    WIP

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,18 @@ use reqwest;
 use scraper::Selector;
 pub const URL: &str = "https://www.toki.co.jp/purchasing/TLIHTML.files/sheet001.htm";
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, PartialOrd)]
 struct ScheduleRow {
     part_number: String,
-    row_len: usize,
     shipments: Vec<f32>,
 }
 
 impl ScheduleRow {
-    fn new(&mut self, name: String, len: usize) -> Self {
-        self.part_number = name.to_string();
-        self.row_len = len;
-        self.shipments = Vec::new();
+    fn new(name: String, quantities: Vec<f32>) -> ScheduleRow {
+        ScheduleRow {
+            part_number: name.to_string(),
+            shipments: quantities,
+        }
     }
 }
 
@@ -28,24 +28,22 @@ fn main() {
     let row_len = dates.len() + 5;
     html.drain(0..(index + row_len));
     let num_rows = (html.len() / row_len) - 1;
-    let mut table: Vec<Vec<String>> = Vec::new();
+    let mut table: Vec<ScheduleRow> = Vec::new();
     for _ in 0..num_rows {
-        let srow: ScheduleRow = ScheduleRow::new("testes".to_string(), row_len);
-        let mut row: Vec<String> = Vec::new();
+        let mut row: ScheduleRow = ScheduleRow::new(String::new(), Vec::new());
         for i in 0..row_len {
             match i {
                 0 => {
-                    let val1 = html[i].clone();
-                    let val1 = &val1[0..val1.find("<").unwrap_or(val1.len())];
-                    row.push(val1.into());
+                    let temp = html[i].clone();
+                    row.part_number = (&temp[0..temp.find("<").unwrap_or(temp.len())]).to_string();
                 }
                 1 | 2 | 3 | 4 => continue,
                 _ => {
-                    let val = html[i].clone();
-                    if val == "�@" {
-                        row.push("".to_string());
+                    let qty = html[i].clone();
+                    if qty == "�@" {
+                        row.shipments.push(0.0);
                     } else {
-                        row.push(html[i].clone());
+                        row.shipments.push(html[i].clone().parse().unwrap());
                     }
                 }
             }
@@ -53,6 +51,7 @@ fn main() {
         table.push(row);
         html.drain(0..row_len);
     }
+    table.sort_by_key(part_number);
     table.iter().for_each(|x| println!("{:?}", x));
 }
 
